cd9b7439477b0323b4679be951e86d992885be83
wvkity <wvkity@gmail.com>
2022-01-07 19:43:14 +0800

    * 新增切换逻辑符接口

---
+++ mybatisx-core/src/main/java/io/github/mybatisx/core/criteria/support/Slot.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2021-Now, wvkity(wvkity@gmail.com).
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package io.github.mybatisx.core.criteria.support;
+
+import io.github.mybatisx.base.constant.LogicSymbol;
+
+/**
+ * AND/OR运算符
+ *
+ * @param <T> 实体类型
+ * @param <C> 子类型
+ * @author wvkity
+ * @created 2022/1/5
+ * @since 1.0.0
+ */
+public interface Slot<T, C extends Slot<T, C>> {
+
+    /**
+     * and
+     *
+     * @return {@code this}
+     */
+    C and();
+
+    /**
+     * or
+     *
+     * @return {@code this}
+     */
+    C or();
+
+    /**
+     * 逻辑符号
+     *
+     * @return {@link LogicSymbol}
+     */
+    LogicSymbol slot();
+
+    /**
+     * 重置
+     *
+     * @return {@code this}
+     */
+    default C reset() {
+        return this.and();
+    }
+
+}
