0f0ebccbc0c89c8978fe64ab4d8bf919aef87d6e
wvkity <wvkity@gmail.com>
2022-02-23 18:00:40 +0800

    * 更改类名

--- mybatisx-annotation/src/main/java/io/github/mybatisx/annotation/MultiTenant.java
+++ mybatisx-annotation/src/main/java/io/github/mybatisx/annotation/MultiTenant.java
@@ -29,5 +29,5 @@ import java.lang.annotation.Target;
  */
 @Target({ElementType.FIELD, ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
-public @interface Tenant {
+public @interface MultiTenant {
 }
