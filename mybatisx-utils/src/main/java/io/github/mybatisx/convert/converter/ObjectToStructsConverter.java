a90d4a46f3f16070a49d86acfd6ec80d66075e7f
wvkity <wvkity@gmail.com>
2023-04-28 09:13:10 +0800

    * 新增其他转换器

---
+++ mybatisx-utils/src/main/java/io/github/mybatisx/convert/converter/ObjectToStructsConverter.java
@@ -0,0 +1,12 @@
+package io.github.mybatisx.convert.converter;
+
+/**
+ * {@link Object} -> 对象列表转换器
+ *
+ * @param <T> 目标类型
+ * @author wvkity
+ * @created 2023/4/27
+ * @since 1.0.0
+ */
+public interface ObjectToStructsConverter<T> extends ObjectToArrayConverter<T> {
+}
